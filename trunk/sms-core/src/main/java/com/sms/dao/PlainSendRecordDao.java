138b3a49ab3a86df9f3fe1ce6d3924cc778b66ad
Administrator <Administrator@192.168.1.2>
2018-03-13 09:56:28 +0800

    修改了PlainSendRecordDao

--- trunk/sms-core/src/main/java/com/sms/dao/PlainSendRecordDao.java
+++ trunk/sms-core/src/main/java/com/sms/dao/PlainSendRecordDao.java
@@ -12,7 +12,5 @@ public interface PlainSendRecordDao extends IGenericDao<PlainSendRecord, Long>{
 	
 	public int updateInitStatusToNew(@Param("recordId")Long recordId, @Param("init")Integer init,@Param("status")Integer status);
 
-	public int updateStatusByReport(@Param("reqMsgId")String reqMsgId,@Param("status")String status,@Param("respMsg")String respMsg,@Param("respTime")String respTime);
-	
 	public int insertBatchList(List<PlainSendRecord> list);
 }
