a2b93e6ccc9786b8b8c7dcb99183f87975152508
Administrator <Administrator@192.168.1.4>
2018-03-16 16:18:53 +0800

    修改了统计功能

--- trunk/sms-dispatch/src/main/java/com/godai/trade/dao/dashboard/TransCountDao.java
+++ trunk/sms-dispatch/src/main/java/com/godai/trade/dao/dashboard/TransCountDao.java
@@ -8,4 +8,6 @@ public interface TransCountDao {
 	public int insert(TransCount transCount);
 	public TransCount getById(@Param("transCountId")Integer transCountId);
 	public TransCount getByAccountNo(@Param("accountNo")Long accountNo);
+	public int update(TransCount transCount);
+	public TransCount getByAccountNoAndStatisticalTime(@Param("accountNo")Long accountNo,@Param("statisticalTime") Long statisticalTime);
 }
--- trunk/sms-dispatch/src/main/java/com/godai/trade/service/dashboard/TransCountService.java
+++ trunk/sms-dispatch/src/main/java/com/godai/trade/service/dashboard/TransCountService.java
@@ -43,6 +43,16 @@ public class TransCountService {
 	 */
 
 	public void genTransCount(MercAccount mercAccount) {
+		//查询一天前的数据
+		TransCountYesterday(mercAccount);
+		//查询两天前的数据
+		TransCountBeforeYesterday(mercAccount);
+		//查询三天前的数据
+		TransCountThreeDaysAgo(mercAccount);
+	}
+
+	//查询一天前的数据并插入统计表
+	private void TransCountYesterday(MercAccount mercAccount) {
 		String yesterdayStart = getYesterdayStart();
 		String yesterdayEnd = getYesterdayEnd();
 		String yesterday = getYesterday();
@@ -67,18 +77,17 @@ public class TransCountService {
 		transCount.setUnknownNum(unknowNum);
 		transCount.setMissionSuccessRate(successRate);
 		transCount.setStatisticalTime(Long.parseLong(yesterday));
-		System.out.println("successRate:" + successRate);
 		transCountDao.insert(transCount);
 	}
-
+	
 	public String getYesterday() {
 		Calendar cal = Calendar.getInstance();
 		cal.add(Calendar.DATE, -1);
 		String yesterday = new SimpleDateFormat("yyyyMMdd").format(cal.getTime());
 		return yesterday;
-
+		
 	}
-
+	
 	public String getYesterdayStart() {
 		Calendar cal = Calendar.getInstance();
 		cal.add(Calendar.DATE, -1);
@@ -87,9 +96,9 @@ public class TransCountService {
 		cal.set(Calendar.SECOND, 0);
 		String yesterdayStart = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ").format(cal.getTime());
 		return yesterdayStart;
-
+		
 	}
-
+	
 	public String getYesterdayEnd() {
 		Calendar cal = Calendar.getInstance();
 		cal.add(Calendar.DATE, -1);
@@ -98,6 +107,132 @@ public class TransCountService {
 		cal.set(Calendar.SECOND, 59);
 		String yesterdayEnd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ").format(cal.getTime());
 		return yesterdayEnd;
+	}
+
+	//查询两天前的数据并更新统计表
+	private void TransCountBeforeYesterday(MercAccount mercAccount) {
+		String beforeyesterdayStart = getBeforeYesterdayStart();
+		String beforeyesterdayEnd = getBeforeYesterdayEnd();
+		String beforeyesterday = getBeforeYesterday();
+		Long accountNo = mercAccount.getAccountNo();
+		TransCount transCount = transCountDao.getByAccountNoAndStatisticalTime(accountNo,Long.parseLong(beforeyesterday));
+		if(transCount !=null){
+			logger.info("TransCountService.genTransCount-Error, Gen TransCount Is Null AccountNo:{},StatisticalTime:{}", accountNo+","+Long.parseLong(beforeyesterday));
+		
+		Integer totalNum = plainSendRecordDao.queryTotalNum(accountNo, beforeyesterdayStart, beforeyesterdayEnd);
+		Integer succNum = plainSendRecordDao.querySuccNum(accountNo, beforeyesterdayStart, beforeyesterdayEnd);
+		Integer failNum = plainSendRecordDao.queryFailNum(accountNo, beforeyesterdayStart, beforeyesterdayEnd);
+		Integer unknowNum = plainSendRecordDao.queryUnknowNum(accountNo, beforeyesterdayStart, beforeyesterdayEnd);
+		
+		if (0 == totalNum) {
+			logger.error("TransCountService.genTransCount-Error, Gen PlainSendRecord Is Null AccountNo:{}", accountNo);
+			return;
+		}
+		Double successRate = getSuccessRate(succNum, totalNum);
+		
+		transCount.setMerchantNameAbbreviation(mercAccount.getMerchantNameAbbreviation());
+		transCount.setAccountNo(accountNo);
+		transCount.setMerchantId(mercAccount.getMerchantId());
+		transCount.setSendNum(totalNum);
+		transCount.setSuccessNum(succNum);
+		transCount.setFailureNum(failNum);
+		transCount.setUnknownNum(unknowNum);
+		transCount.setMissionSuccessRate(successRate);
+		transCount.setStatisticalTime(Long.parseLong(beforeyesterday));
+		transCountDao.update(transCount);
+		}
+	}
+
+	public String getBeforeYesterday() {
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -2);
+		String yesterday = new SimpleDateFormat("yyyyMMdd").format(cal.getTime());
+		return yesterday;
+
+	}
+
+	public String getBeforeYesterdayStart() {
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -2);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		String beforeYesterdayStart = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ").format(cal.getTime());
+		return beforeYesterdayStart;
+
+	}
+
+	public String getBeforeYesterdayEnd() {
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -2);
+		cal.set(Calendar.HOUR_OF_DAY, 23);
+		cal.set(Calendar.MINUTE, 59);
+		cal.set(Calendar.SECOND, 59);
+		String beforeyesterdayEnd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ").format(cal.getTime());
+		return beforeyesterdayEnd;
+	}
+	
+	//查询三天前的数据并更新统计表
+	private void TransCountThreeDaysAgo(MercAccount mercAccount) {
+		String threeDaysAgoStart = getThreeDaysAgoStart();
+		String threeDaysAgoEnd = getThreeDaysAgoEnd();
+		String threeDaysAgo = getThreeDaysAgo();
+		Long accountNo = mercAccount.getAccountNo();
+		TransCount transCount = transCountDao.getByAccountNoAndStatisticalTime(accountNo,Long.parseLong(threeDaysAgo));
+		if(transCount !=null){
+			logger.info("TransCountService.genTransCount-Error, Gen TransCount Is Null AccountNo:{},StatisticalTime:{}", accountNo+","+Long.parseLong(threeDaysAgo));
+		
+		Integer totalNum = plainSendRecordDao.queryTotalNum(accountNo, threeDaysAgoStart, threeDaysAgoEnd);
+		Integer succNum = plainSendRecordDao.querySuccNum(accountNo, threeDaysAgoStart, threeDaysAgoEnd);
+		Integer failNum = plainSendRecordDao.queryFailNum(accountNo, threeDaysAgoStart, threeDaysAgoEnd);
+		Integer unknowNum = plainSendRecordDao.queryUnknowNum(accountNo, threeDaysAgoStart, threeDaysAgoEnd);
+		
+		if (0 == totalNum) {
+			logger.error("TransCountService.genTransCount-Error, Gen PlainSendRecord Is Null AccountNo:{}", accountNo);
+			return;
+		}
+		Double successRate = getSuccessRate(succNum, totalNum);
+		
+		transCount.setMerchantNameAbbreviation(mercAccount.getMerchantNameAbbreviation());
+		transCount.setAccountNo(accountNo);
+		transCount.setMerchantId(mercAccount.getMerchantId());
+		transCount.setSendNum(totalNum);
+		transCount.setSuccessNum(succNum);
+		transCount.setFailureNum(failNum);
+		transCount.setUnknownNum(unknowNum);
+		transCount.setMissionSuccessRate(successRate);
+		transCount.setStatisticalTime(Long.parseLong(threeDaysAgo));
+		transCountDao.update(transCount);
+		}
+	}
+	
+	public String getThreeDaysAgo() {
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -3);
+		String threeDaysAgo = new SimpleDateFormat("yyyyMMdd").format(cal.getTime());
+		return threeDaysAgo;
+		
+	}
+	
+	public String getThreeDaysAgoStart() {
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -3);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		String threeDaysAgoStart = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ").format(cal.getTime());
+		return threeDaysAgoStart;
+		
+	}
+	
+	public String getThreeDaysAgoEnd() {
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -3);
+		cal.set(Calendar.HOUR_OF_DAY, 23);
+		cal.set(Calendar.MINUTE, 59);
+		cal.set(Calendar.SECOND, 59);
+		String threeDaysAgoEnd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ").format(cal.getTime());
+		return threeDaysAgoEnd;
 	}
 
 	public Double getSuccessRate(Integer succNum, Integer totalNUM) {
